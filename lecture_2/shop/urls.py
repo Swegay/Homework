0057a20c02cc7f993c537a9374992944983e6921
bleij <kapin@inbox.ru>
2024-02-03 08:41:35 +0600

    managed movements

--- lecture_2/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from .views import get_ingex_page, get_product_details
-
-urlpatterns = [
-    path('', get_ingex_page),
-    path('<int:pk>', get_product_details)
-]
